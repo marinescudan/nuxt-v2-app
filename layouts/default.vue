3b38fc83dbf0f9febe0291bc11ce6a4558c8b663
Dan Marinescu <marinescudan@users.noreply.github.com>
2023-10-05 17:08:32 +0000

    Adds Home componenet and tests to get full covareg

---
+++ layouts/default.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="default-layout">
+        <nuxt />
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    components: {  },
+    head() {
+      return {
+        title: 'Blank layout',
+      }
+    },
+    computed: {},
+  }
+  </script>
+  
+  <style>
+
+  @media print {
+
+  }
+  </style>
+  
